342c9f5364bd7d4e6ed024bf408e286f0453c47e
lelethu <lelethumlinjana71@gmail.com>
2023-06-06 16:16:34 +0200

    message

--- mediApp/src/app/components/homepage/homepage.component.spec.ts
+++ mediApp/src/app/components/homepage/homepage.component.spec.ts
@@ -8,9 +8,9 @@ describe('HomepageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [HomepageComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
