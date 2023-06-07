722f2e2dc80b52c442ecb1c2b2ab4979f0ec064f
lelethu <lelethumlinjana71@gmail.com>
2023-06-07 16:34:03 +0200

    message

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// pool.query("SELECT NOW()", (err, res) => {
+//   if (err) {
+//     console.error("Error connecting to the database", err);
+//   } else {
+//     console.log("Successfully connected to the database");
+//   }
+
+// });
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+    console.log("Successfully connected to the database")
+  })
+
+  
+
+  
